f6b52dcac33d6255eb82a89836d31ce5d04a4407
gh0stwizard <vitaliy.tokarev@gmail.com>
2015-07-07 17:42:38 +0300

    use strict pragma + warnings

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # Perl UDP Port Scanner #
 
-This is a fork of original Perl UDP Port Scanner written by 
+This is a fork of the original Perl UDP Port Scanner written by 
 [Ofer Koren](http://korenofer.blogspot.ru/2009/02/simple-udp-port-scanner-in-perl-icmp_14.html).
+
+
+## Usage ##
+
+```
+Usage: udp-scan.pl [[ host ] [ port | start_port-end_port ] [timeout]]
+```
+
+For instance, to scan the port range from 1 to 1024 on example.com host:
+
+```
+shell> perl udp-scan.pl example.com 1-1024
+```
--- udp-scan.pl
+++ udp-scan.pl
@@ -1,75 +1,85 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 #
-# http://korenofer.blogspot.ru/2009/02/simple-udp-port-scanner-in-perl-icmp_14.html
+# Credits:
+#   http://korenofer.blogspot.ru/2009/02/simple-udp-port-scanner-in-perl-icmp_14.html
 #
 
+use strict;
 use Net::Ping;
 use IO::Select;
 use IO::Socket::INET;
 
+
 # IP address to scan
-my $ip = $ARGV[0] || '10.0.0.1';
+my $host = $ARGV[0] || '127.0.0.1';
 my ( $first_port, $last_port ) = split( /-/, $ARGV[1] || '1-65535', 2 );
 
-# First we will send a ping to make sure the scanned host is exist
-my $p = Net::Ping->new( "icmp", 1, 64 );
-
-if ( $p->ping($ip) ) {
-  print "$ip ping-pong finished successfully\n";
-} else {
-  print "$ip did not answer ping\n";
-  exit 5;
-}
-
 # Time  to wait for the "destination unreachable" package.
-my $icmp_timeout = 2;
-
-# Create the icmp socket for the "destination unreachable" packages  
-$icmp_sock = IO::Socket::INET->new( Proto => "icmp" );
-$read_set = IO::Select->new();
-$read_set->add( $icmp_sock );
+my $icmp_timeout = int( $ARGV[2] ) || 2;
 
 # Buffer to send via UDP
 my $hello = "Hello\n";
 
+
+# First we will send a ping to make sure the scanned host is exist
+my $ping = Net::Ping->new( "icmp", 1, 64 );
+
+if ( defined $ping and $ping->ping( $host ) ) {
+  print STDERR "$host recieved ICMP echo response, start scanning...\n";
+} else {
+  print STDERR "$host did not sent a response via ICMP\n";
+  exit 1;
+}
+
+# Create the icmp socket for the "destination unreachable" packages  
+my $icmp = IO::Socket::INET->new( Proto => "icmp" );
+my $select = IO::Select->new();
+$select->add( $icmp );
+
+
 # Scan all the ports .....
-for ( $i = $first_port ; $i <= ( $last_port || $first_port ) ; $i++ ) {      # Create UDP socket to the remote  host and port
-    my $udp = IO::Socket::INET->new
-      (
-        PeerAddr => $ip,
-        PeerPort => $i,
-        Proto    => "udp"
-      )
-    ;
+for ( my $port = int( $first_port );
+      $port <= int( $last_port || $first_port );
+      $port++ )
+{
+  # Create UDP socket to the remote  host and port
+  my $udp = IO::Socket::INET->new
+    (
+      PeerAddr => $host,
+      PeerPort => $port,
+      Proto    => "udp",
+    ) or die "Could not create UDP socket: $!";
 
-    # Send the buffer and close the UDP socket.
-    $udp->send( $hello );
+  # Send the buffer and close the UDP socket.
+  $udp->send( $hello );
 
-    # Set the arrival flag.
-    my $icmp_arrived = 0;
+  # Set the arrival flag.
+  my $icmp_arrived = 0;
 
-    # For every socket we had received packets (In our case only one - icmp_socket)
-    my @done = $read_set->can_read( $icmp_timeout );
+  # For every socket we had received packets 
+  # (In our case only one - icmp_socket)
+  my @done = $select->can_read( $icmp_timeout );
 
-    for my $socket ( @done ) {
-      # If we have captured an icmp packages, Its probably "destination unreachable"
-      if ( $socket == $icmp_sock ) {
-        # Set the flag and clean the socket buffers
-        $icmp_arrived = 1;
-        $icmp_sock->recv( $buffer, 50, 0 );
-        last;
-      }
+  for my $socket ( @done ) {
+    # If we have captured an icmp packages,
+    # Its probably "destination unreachable"
+    if ( $socket == $icmp ) {
+      # Set the flag and clean the socket buffers
+      $icmp_arrived = 1;
+      $icmp->recv( my $buffer, 50, 0 );
+      last;
     }
+  }
 
-    if ( $icmp_arrived == 0 ) {
-      printf "\t%-5d opened\n", $i;
-    }
+  if ( $icmp_arrived == 0 ) {
+    printf "\t%-5d opened\n", $port;
+  }
 
-    $udp->close();
+  $udp->close();
 }
 
 # Close the icmp sock
-$icmp_sock->close();
+$icmp->close();
 exit 0;
 
