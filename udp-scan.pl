bde89499264c502da030223451946949815d42f7
gh0stwizard <vitaliy.tokarev@gmail.com>
2015-07-08 11:33:37 +0300

    convert into function

--- udp-scan.pl
+++ udp-scan.pl
@@ -15,8 +15,8 @@ use IO::Socket::INET;
 my $host = $ARGV[0] || '127.0.0.1';
 my ( $first_port, $last_port ) = split( /-/, $ARGV[1] || '1-65535', 2 );
 
-# Time  to wait for the "destination unreachable" package.
-my $icmp_timeout = int( $ARGV[2] || 2 );
+# Time to wait for the "destination unreachable" package.
+my $timeout = int( $ARGV[2] || 2 );
 
 # Buffer to send via UDP
 my $hello = "Hello\n";
@@ -32,17 +32,20 @@ if ( defined $ping and $ping->ping( $host ) ) {
   exit 1;
 }
 
-# Create the icmp socket for the "destination unreachable" packages  
-my $icmp = IO::Socket::INET->new( Proto => "icmp" );
-my $select = IO::Select->new();
-$select->add( $icmp );
+for my $port (int( $first_port) .. int( $last_port || $first_port ) ) {
+  &check_udp( $host, $port )
+    and printf "\t%-5d opened\n", $port;
+}
 
+exit 0;
 
-# Scan all the ports .....
-for ( my $port = int( $first_port );
-      $port <= int( $last_port || $first_port );
-      $port++ )
-{
+sub check_udp($$) {
+  my ( $host, $port ) = @_;
+  
+
+  # Create the icmp socket for the "destination unreachable" packages  
+  my $icmp = IO::Socket::INET->new(Proto   => "icmp" );
+
   # Create UDP socket to the remote  host and port
   my $udp = IO::Socket::INET->new
     (
@@ -53,13 +56,17 @@ for ( my $port = int( $first_port );
 
   # Send the buffer and close the UDP socket.
   $udp->send( $hello );
+  
+  my $select = IO::Select->new();
+  $select->add( $icmp );
+  $select->add( $udp );
 
   # Set the arrival flag.
   my $icmp_arrived = 0;
 
   # For every socket we had received packets 
   # (In our case only one - icmp_socket)
-  my @done = $select->can_read( $icmp_timeout );
+  my @done = $select->can_read( $timeout );
 
   for my $socket ( @done ) {
     # If we have captured an icmp packages,
@@ -67,19 +74,13 @@ for ( my $port = int( $first_port );
     if ( $socket == $icmp ) {
       # Set the flag and clean the socket buffers
       $icmp_arrived = 1;
-      $icmp->recv( my $buffer, 50, 0 );
-      last;
+      $icmp->recv( my $packet, 64, 0 );
+      return 0;
     }
   }
 
-  if ( $icmp_arrived == 0 ) {
-    printf "\t%-5d opened\n", $port;
-  }
+  close( $icmp );
+  close( $udp );
 
-  $udp->close();
+  return $icmp_arrived ? 0 : 1;
 }
-
-# Close the icmp sock
-$icmp->close();
-exit 0;
-
